f5f00b1185d543fecaf09ba5cabf005b84bbb506
clearcreations <findclarity@clearcreations.io>
2023-07-06 21:15:32 -0400

    prior to storing data

--- app/Progress.jsx
+++ app/Progress.jsx
@@ -1,5 +1,14 @@
 import React, {useState, useEffect} from 'react'
-
+import Image from 'next/image'
+import brandingIcon from '../public/Assets/branding-icon.png'
+import brandingFloatTop from '../public/Assets/branding-float-1.png'
+import brandingFloatBottom from '../public/Assets/branding-float-2.png'
+import uiuxIcon from '../public/Assets/uiux-icon.png'
+import uiuxFloatTop from '../public/Assets/uiux-float-1.png'
+import uiuxFloatBottom from '../public/Assets/uiux-float-2.png'
+import digitalMarketingIcon from '../public/Assets/digital-marketing-icon.png'
+import digitalMarketingFloatTop from '../public/Assets/digital-marketing-float-1.png'
+import digitalMarketingFloatBottom from '../public/Assets/digital-marketing-float-2.png'
 
 const Progress = () => {
 
@@ -11,13 +20,44 @@ const Progress = () => {
         setProgressHeight(heightPercentage);
     }
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setShowcontent((prevContent) => (prevContent % 3) + 1);
-        }, 2000);
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //         setShowcontent((prevContent) => (prevContent % 3) + 1);
+    //     }, 2000);
 
-        return () => clearInterval(interval);
-    }, []);
+    //     return () => clearInterval(interval);
+    // }, []);   
+
+    const contentItems = [
+        {
+          id: 1,
+          className: 'branding-content',
+          floatTopImage: brandingFloatTop,
+          iconImage: brandingIcon,
+          heading: 'Standout Branding.',
+          offers: ['Brand Strategy', 'Brand Identity', 'Brand Messaging'],
+          floatBottomImage: brandingFloatBottom
+        },
+        {
+          id: 2,
+          className: 'uiux-content',
+          floatTopImage: uiuxFloatTop,
+          iconImage: uiuxIcon,
+          heading: 'Drive Digital Success.',
+          offers: ['Product Strategy', 'UI/UX Design', 'Web & Mobile'],
+          floatBottomImage: uiuxFloatBottom
+        },
+        {
+          id: 3,
+          className: 'digmar-content',
+          floatTopImage: digitalMarketingFloatTop,
+          iconImage: digitalMarketingIcon,
+          heading: 'Unleash Growth.',
+          offers: ['Social Media', 'Pay-Per-Click', 'SEO', 'Content Marketing', 'Email Marketing'],
+          floatBottomImage: digitalMarketingFloatBottom
+        }
+    ];
+
     
   return (
     <div className='service-container'>
@@ -31,13 +71,71 @@ const Progress = () => {
         </div>
         <div className="service-contents">
                 <div className={showContent === 1 ? 'service-content content1 active' : 'service-content content1'}>
-                    1
+                    <div className="branding-content">
+                    <div className="float-top">
+                                {/* <Image src={brandingFloatTop} className='float-top' alt={`Clear Creations | Restaurant Brand Identity | Restaurant Branding Agency`} /> */}
+                            </div>
+                            <div className="icon-container">
+                                {/* <Image src={brandingIcon} className='icon' alt={`Clear Creations | Charlotte Branding Services`}/> */}
+                            </div>
+                            <div className="service-text-container">
+                                {/* <h2 className="service-heading">Standout Branding.</h2>
+                                <div className="service-offer">
+                                    <p className="offer">Brand Strategy</p>
+                                    <p className="offer">Brand Identity</p>
+                                    <p className="offer">Brand Messaging</p>
+                                </div> */}
+                            </div>
+                            <div className="float-bottom">
+                                {/* <Image src={brandingFloatBottom} className='float-btm' alt={`Clear Creations | Saas Brand Identity | Saas Branding Agency`} /> */}
+                            </div>
+                    </div>
                 </div>
                 <div className={showContent === 2 ? 'service-content content2 active' : 'service-content content2'}>
-                    2
+                    <div className="uiux-content">
+                    <div className="float">
+                                {/* <Image src={uiuxFloatBottom} className='float-btm' alt={`Clear Creations | Restaurant Web Design & Mobile App Design | Restaurant Web Design & Mobile App Design Agency`} /> */}
+                            </div>
+                            <div className="icon-container">
+                                {/* <Image src={uiuxIcon} className='icon' alt={`Clear Creations | Charlotte Web Design & Mobile App Design Services`}/> */}
+                            </div>
+                            <div className="service-text-container">
+                                {/* <h2 className="service-heading">Drive Digital Success.</h2>
+                                <div className="service-offer">
+                                    <p className="offer">Product Strategy</p>
+                                    <p className="offer">UI/UX Design</p>
+                                    <p className="offer">Web & Mobile</p>
+                                </div> */}
+                            </div>
+                            <div className="float">
+                                {/* <Image src={uiuxFloatTop} className='float-top' alt={`Clear Creations | Saas Web Design & Mobile App Design | Saas Web Design & Mobile App Design Agency`} /> */}
+                            </div>
+                    </div>
                 </div>
                 <div className={showContent === 3 ? 'service-content content3 active' : 'service-content content3'}>
-                    3
+                    <div className="digmar-content">
+                        <div className="float">
+                            <div className="float-top"></div>
+                                {/* <Image src={digitalMarketingFloatTop} className='float-top' alt={`Clear Creations | Restaurant Digital Marketing | Restaurant Digital Marketing Agency`} /> */}
+                        </div>
+                        <div className="icon-container">
+                            {/* <Image src={digitalMarketingIcon} className='icon' alt={`Clear Creations | Charlotte Digital Marketing Services`}/> */}
+                        </div>
+                        <div className="service-text-container">
+                            {/* <h2 className="service-heading">Unleash Growth.</h2>
+                            <div className="service-offer">
+                                <p className="offer">Social Media</p>
+                                <p className="offer">Pay-Per-Click</p>
+                                <p className="offer">SEO</p>
+                                <p className="offer">Content Marketing</p>
+                                <p className="offer">Email Marketing</p>
+                            </div> */}
+                        </div>
+                        <div className="float">
+                            <div className="float-btm"></div>
+                            {/* <Image src={digitalMarketingFloatBottom} className='float-btm' alt={`Clear Creations | Saas Digital Marketing | Saas Digital Marketing Agency`} /> */}
+                        </div>
+                    </div>
                 </div>
         </div>
     </div>
--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import {useRef, useState} from 'react'
 import emailjs from '@emailjs/browser'
 import './page.css'
@@ -31,12 +31,71 @@ const page = () => {
 				}
 			);
 	};
+
+	const logos = [
+		'/Assets/ash-and-barrel-logo.png',
+		'/Assets/cross-country-healthcare-logo.png',
+		'/Assets/order-my-maid-logo.png',
+		'/Assets/robbin-n-williams-logo.png',
+		'/Assets/scadc-actual.png',
+		'/Assets/sd-botox-logo.png',
+		'/Assets/smartboost-logo.png',
+		'/Assets/soundful-logo.png',
+		'/Assets/trinity-logo.png',
+		'/Assets/veloce-actual.png',
+		'/Assets/wellness-med-spa-logo.png',
+		'/Assets/wonf-logo.png'
+	]
+
+	const [currentLogoIndex, setCurrentLogoIndex] = useState(0);
+	
+	useEffect(() => {
+		//Change logo every 2 seconds
+		const interval = setInterval(() => {
+			setCurrentLogoIndex((prevIndex) => (prevIndex + 1) % logos.length);
+		}, 3000);
+
+		//Clear function
+		return () => clearInterval(interval)
+	}, []);
+
 	return (
 		<div className='content'>
 			<div className="content-container">
+				
 				<div className="content-left">
 					<div className="content-inner">
 						<Progress/>
+						<div className="social-proof">
+      						<div className="logo-container">
+        						{logos.map((logo, index) => (
+								<div
+									key={index}
+									className={`logo ${index === currentLogoIndex ? 'active' : ''}`}
+								>
+									<Image 
+										src={logo}
+										alt={`Logo ${index + 1}`}
+										width={90}
+										height={25}
+								/>
+								</div>
+								))}
+								{logos.map((logo, index) => (
+								<div
+									key={index}
+									className={`logo ${index === currentLogoIndex ? 'active' : ''}`}
+								>
+									<Image 
+										src={logo}
+										alt={`Logo ${index + 1}`}
+										width={90}
+										height={25}
+								/>
+								</div>
+								))}
+							</div>
+						</div>
 					</div>
 				</div>
 				<div className="content-right">
